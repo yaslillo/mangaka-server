b7aa9cc94ab04d09d28897f4930815764102cb8f
yas lillo <yasnalillo27@gmail.com>
2022-02-24 05:45:43 -0300

    fix: logout

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -39,7 +39,7 @@ authRouter.post<{}, {}>("/local/login", (req, res, next) => {
       if (err) throw err;
       if (!user) return res.status(404).send("No user exists");
       else {
-        req.logIn(user, (err) => {
+        req.login(user, (err) => {
           if (err) throw err;
           return res.send(user) 
         });
@@ -49,13 +49,10 @@ authRouter.post<{}, {}>("/local/login", (req, res, next) => {
 });
 
 authRouter.get("/logout", (req, res) => {
-  // console.log(req);
-  if (req.user) {
-    // console.log("logout");
+  try {
     req.logout();
-    res.send("Logout success");
-  } else {
-    // console.log("no logout");
-    res.status(400).send({ msg: "User not logged in" });
+  } catch (e) { 
+    console.log(e);
   }
+  return res.status(200).send("Logout success");
 });
