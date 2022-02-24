c10fdd4209bdd3cf3df7f9457311e3205151c3d8
yas lillo <yasnalillo27@gmail.com>
2022-02-24 04:27:20 -0300

    feature: add cors middleware

--- src/app.ts
+++ src/app.ts
@@ -17,22 +17,11 @@ const localStrategy = require("passport-local").Strategy;
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
-
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cors({
-//   origin: "http://localhost:3000",
-//   credentials: true
-// }));
+app.use(cors({
+  origin: ["https://mangaka-client.herokuapp.com", "http://localhost:3000"],
+  credentials: true
+}));
 
 app.use(morgan("dev"));
 app.use(express.json());
